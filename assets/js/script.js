a8d0ff5df0305adddd504041aeb4a5a57ec3c5ea
Chanjnint21 <limchanjnint@gmail.com>
2022-10-04 17:08:01 +0700

    Added Request Page (Admin)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,46 @@
+//----------------------------------------------------- hide/show side bar ---------------------------------------------
+
 (function() {
     document.getElementById("sidebarCollapse").addEventListener('click', (e) => {
         document.getElementById("sidebar").classList.toggle("active");
         document.getElementById("body").classList.toggle("active");
     });
 })();
+
+//----------------------------------------------------- button change color ---------------------------------------------
+
+function btn1(){
+    var elem = document.getElementById("Apr-btn1");
+    if (elem.value=="Reject"){
+        elem.value = "Approve";
+        elem.style.color='green';
+    } 
+    else {
+        elem.value = "Reject";
+        elem.style.color='red';
+    }
+}
+
+function btn2(){
+    var elem = document.getElementById("Apr-btn2");
+    if (elem.value=="Reject"){
+        elem.value = "Approve";
+        elem.style.color='green';
+    } 
+    else {
+        elem.value = "Reject";
+        elem.style.color='red';
+    }
+}
+
+function btn3(){
+    var elem = document.getElementById("Apr-btn3");
+    if (elem.value=="Reject"){
+        elem.value = "Approve";
+        elem.style.color='green';
+    } 
+    else {
+        elem.value = "Reject";
+        elem.style.color='red';
+    }
+}
