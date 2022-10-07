05a64d04cd9e0a69d01acce20e459abf1262efdf
Chanjnint21 <limchanjnint@gmail.com>
2022-10-07 16:57:44 +0700

    Fixed remove the row script

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,41 +11,44 @@
 
 function btn1(){
     var elem = document.getElementById("Apr-btn1");
-    if (elem.value=="Reject"){
-        elem.value = "Approve";
-        elem.style.color='green';
-    } 
-    else {
-        elem.value = "Reject";
-        elem.style.color='red';
-        document.getElementsByTagName("tr")[1].remove();
-    }
+    document.getElementsByTagName("tr")[1].remove();
+    // if (elem.value=="Reject"){
+    //     elem.value = "Approve";
+    //     elem.style.color='green';
+    // } 
+    // else {
+    //     elem.value = "Reject";
+    //     elem.style.color='red';
+    //     document.getElementsByTagName("tr")[1].remove();
+    // }
 }
 
 function btn2(){
     var elem = document.getElementById("Apr-btn2");
-    if (elem.value=="Reject"){
-        elem.value = "Approve";
-        elem.style.color='green';
-    } 
-    else {
-        elem.value = "Reject";
-        elem.style.color='red';
-        document.getElementsByTagName("tr")[2].remove();
-    }
+    document.getElementsByTagName("tr")[2].remove();
+    // if (elem.value=="Reject"){
+    //     elem.value = "Approve";
+    //     elem.style.color='green';
+    // } 
+    // else {
+    //     elem.value = "Reject";
+    //     elem.style.color='red';
+    //     document.getElementsByTagName("tr")[2].remove();
+    // }
 }
 
 function btn3(){
     var elem = document.getElementById("Apr-btn3");
-    if (elem.value=="Reject"){
-        elem.value = "Approve";
-        elem.style.color='green';
-    } 
-    else {
-        elem.value = "Reject";
-        elem.style.color='red';
-        document.getElementsByTagName("tr")[3].remove();
-    }
+    document.getElementsByTagName("tr")[3].remove();
+    // if (elem.value=="Reject"){
+    //     elem.value = "Approve";
+    //     elem.style.color='green';
+    // } 
+    // else {
+    //     elem.value = "Reject";
+    //     elem.style.color='red';
+    //     document.getElementsByTagName("tr")[3].remove();
+    // }
 }
 
 var tbl = document.getElementById('x');
@@ -53,7 +56,5 @@ if(tbl.rows.length==0){
     document.getElementById("text").innerHTML = 'No Request';
 }
 
-document.getElementById('#myModal').on('shown.bs.modal', function () {
-    document.getElementById('#myInput').trigger('focus')
-  })
+
 
