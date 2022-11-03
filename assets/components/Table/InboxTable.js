359e2d8864e1779bb1fa42c77460bcc83a07a2e8
Chanjnint21 <limchanjnint@gmail.com>
2022-11-03 15:22:40 +0700

    Added script to load the value into modal

--- assets/components/Table/InboxTable.js
+++ assets/components/Table/InboxTable.js
@@ -1,38 +1,66 @@
 $(document).ready(function () {
+    document.getElementById("req_fname").innerHTML = localStorage.getItem("firstname");
+    document.getElementById("req_lname").innerHTML = localStorage.getItem("lastname");
+    document.getElementById("req_phone").innerHTML = localStorage.getItem("phone");
+    document.getElementById("req_email").innerHTML = localStorage.getItem("email");
+    document.getElementById("req_totalleave").innerHTML = localStorage.getItem("TotalLeave") + "days";
+    document.getElementById("req_date").innerHTML = localStorage.getItem("From") + " to " + localStorage.getItem("To");
+    document.getElementById("req_reason").innerHTML = localStorage.getItem("reason");
+    document.getElementById("req_attachment").innerHTML = localStorage.getItem("attachment");
+
+    // elem.classList.remove('show');
+    //             // this force-restarts the CSS animation
+    //             void elem.offsetWidth;
+    //         elem.classList.add('show');
     var t = $('#display').DataTable();
-    var num1 = 1;
-    var num2 = 1;
-    var num3 = 2;
     var firstname = localStorage.getItem("firstname");
     var lastname = localStorage.getItem("lastname");
     var title = localStorage.getItem("title");
     var subdate = localStorage.getItem("submitdate");
-    var reviewPEN = "<span type='button' class='material-icons' data-toggle='modal' data-target='#modal-pending'>_reviews</span>";
-    var reviewAPP = "<span type='button' class='material-icons' data-toggle='modal' data-target='#modal-reviewed'>_reviews</span>";
-    var reviewREJ = "<span type='button' class='material-icons' data-toggle='modal' data-target='#modal-reviewed'>_reviews</span>";
+    var pending_review = "<span type='button' class='material-icons' data-toggle='modal' data-target='#modal-pending'>_reviews</span>";
+    var Already_review = "<span type='button' class='material-icons' data-toggle='modal' data-target='#modal-reviewed'>_reviews</span>";
     var status = localStorage.getItem("status");
-
-    $('#btnn').on('click', function () {
-        if (localStorage.getItem("firstname") === null){
-            elem.classList.remove('show');
-                // this force-restarts the CSS animation
-                void elem.offsetWidth;
-            elem.classList.add('show');
-                // setTimeout(alert("no New Request !"), 5000);
-        } else {
-            elem.classList.remove('show');
-                // this force-restarts the CSS animation
-                void elem.offsetWidth;
-            elem.classList.add('show');
-            t.row.add([num1, firstname, lastname, title, subdate, reviewPEN, status]).draw(false);
-        }
-    });
+    // $('#btnn').on('click', function () {
+    //     if (localStorage.getItem("firstname") === null){
+    //         elem.classList.remove('show');
+    //             // this force-restarts the CSS animation
+    //             void elem.offsetWidth;
+    //         elem.classList.add('show');
+    //             // setTimeout(alert("no New Request !"), 5000);
+    //     } else {
+    //         elem.classList.remove('show');
+    //             // this force-restarts the CSS animation
+    //             void elem.offsetWidth;
+    //         elem.classList.add('show');
+    //         t.row.add([num1, firstname, lastname, title, subdate, reviewPEN, status]).draw(false);
+    //     }
+    // });
+    var approve = 'APPROVE'; 
+    var reject = 'REJECT';
+    if (status == null){
+        var num2 = 1;
+        var num3 = 2;
+        var num4 = 3;
+        var num5 = 4;
+        var num6 = 5;
+        var num7 = 6;
+    } else {
+        var num1 = 1;
+        var num2 = 2;
+        var num3 = 3;
+        var num4 = 4;
+        var num5 = 5;
+        var num6 = 6;
+        var num7 = 7;
+        t.row.add([num1, firstname, lastname, title, subdate, pending_review, status]).draw(false);
+    }
 
     // Automatically add a first row of data
-    t.row.add(['2', 'Sokha', "chan", "Phcum Ben", "12/09/2022", reviewAPP, "PENDING"]).draw(false);
-    t.row.add(['3', 'MArk', "jackson", "title", "12/09/2022", reviewAPP, "PENDING"]).draw(false);
-    t.row.add(['4', 'Thida', "karl", "Phcum Ben", "12/09/2022", reviewAPP, "PENDING"]).draw(false);
-    t.row.add(['5', 'Daro', "krosa", "title", "12/09/2022", reviewAPP, "PENDING"]).draw(false);
-    t.row.add(['6', 'vireak', "chan", "Phcum Ben", "12/09/2022", reviewAPP, "PENDING"]).draw(false);
-    t.row.add(['7', 'Kosal', "vicheka", "title", "12/09/2022", reviewAPP, "PENDING"]).draw(false);
+    t.row.add([num2, 'Sokha', "chan", "Phcum Ben", "12/07/2022", Already_review, approve]).draw(false);
+    t.row.add([num3, 'MArk', "jackson", "title", "12/06/2022", Already_review, approve]).draw(false);
+    t.row.add([num4, 'Thida', "karl", "Phcum Ben", "12/07/2022", Already_review, approve]).draw(false);
+    t.row.add([num5, 'Daro', "krosa", "title", "12/05/2022", Already_review, reject]).draw(false);
+    t.row.add([num6, 'vireak', "chan", "Phcum Ben", "12/09/2022", Already_review, reject]).draw(false);
+    t.row.add([num7, 'Kosal', "vicheka", "title", "12/09/2022", Already_review, reject]).draw(false);
+    
 });
