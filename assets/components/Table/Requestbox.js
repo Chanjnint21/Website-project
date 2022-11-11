e8e65dec47c27653c823a7552beeaf31c17297c1
Chanjnint21 <limchanjnint@gmail.com>
2022-11-11 16:42:41 +0700

    added script to select row and delete

--- assets/components/Table/Requestbox.js
+++ assets/components/Table/Requestbox.js
@@ -18,40 +18,23 @@ $(document).ready(function () {
     var status = "<button data-toggle='modal' data-target='#comfirm-modal' data-title='change status' ><span id='demo1' value=''>PENDING</span></button>";
     var approve = "<span style='color: green;'>APPROVE</span>"; 
     var reject = "<span style='color: red;'>REJECT</span>";
-    if (localstatus == null){
-        var num2 = 1;
-        var num3 = 2;
-        var num4 = 3;
-        var num5 = 4;
-        var num6 = 5;
-        var num7 = 6;
-        var num8 = 7;
-        var num9 = 8;
-        var num10 = 9;
-    } else {
-        var num1 = 1;
-        var num2 = 2;
-        var num3 = 3;
-        var num4 = 4;
-        var num5 = 5;
-        var num6 = 6;
-        var num7 = 7;
-        var num8 = 8;
-        var num9 = 9;
-        var num10 = 10;
-        t.row.add([num1, firstname, lastname, title, subdate, review, status]).draw(false);
-    }
+    var checkbox= "<input type='checkbox' id='Check' onclick='showremovebtn()' class='select'/>";
+    
+    if (localstatus !== null){
+        t.row.add([checkbox, firstname, lastname, title, subdate, review, status]).draw(false);
+    } 
 
     // Automatically add a first row of data
-    t.row.add([num2, 'Sokha', "chan", "Phcum Ben", "12/07/2022", review, approve]).draw(false);
-    t.row.add([num3, 'MArk', "jackson", "title", "12/06/2022", review, approve]).draw(false);
-    t.row.add([num4, 'Thida', "karl", "Phcum Ben", "12/07/2022", review, approve]).draw(false);
-    t.row.add([num5, 'Daro', "krosa", "title", "12/05/2022", review, reject]).draw(false);
-    t.row.add([num6, 'vireak', "chan", "Phcum Ben", "12/09/2022", review, reject]).draw(false);
-    t.row.add([num7, 'Kosal', "vicheka", "title", "13/07/2022",review, reject]).draw(false);
-    t.row.add([num8, 'Kjoool', "Rosa", "title", "24/05/2022", review, reject]).draw(false);
-    t.row.add([num9, 'forlto', "chanao", "Phcum Ben", "31/09/2022", review, reject]).draw(false);
-    t.row.add([num10, 'candaal', "ghiloo", "title", "12/10/2022", review, reject]).draw(false);
+
+    t.row.add([checkbox, 'Sokha', "chan", "Phcum Ben", "12/07/2022", review, approve]).draw(false);
+    t.row.add([checkbox, 'MArk', "jackson", "title", "12/06/2022", review, approve]).draw(false);
+    t.row.add([checkbox, 'Thida', "karl", "Phcum Ben", "12/07/2022", review, approve]).draw(false);
+    t.row.add([checkbox, 'Daro', "krosa", "title", "12/05/2022", review, reject]).draw(false);
+    t.row.add([checkbox, 'vireak', "chan", "Phcum Ben", "12/09/2022", review, reject]).draw(false);
+    t.row.add([checkbox, 'Kosal', "vicheka", "title", "13/07/2022",review, reject]).draw(false);
+    t.row.add([checkbox, 'Kjoool', "Rosa", "title", "24/05/2022", review, reject]).draw(false);
+    // t.row.add([checkbox, 'forlto', "chanao", "Phcum Ben", "31/09/2022", review, reject]).draw(false);
+    // t.row.add([checkbox, 'candaal', "ghiloo", "title", "12/10/2022", review, reject]).draw(false);
     
 
     document.getElementById("Reject").addEventListener("click", Rbtn)
@@ -81,5 +64,29 @@ function status1(){
         elem.value = "Reject";
         elem.style.color='red';
         document.getElementById("demo1").innerHTML= "Reject";
+    }
+}
+
+
+function DeleteRow() {
+    document.querySelectorAll('#display .select:checked').forEach(e => {
+      e.parentNode.parentNode.remove();
+    });
+}
+
+document.getElementById('all').addEventListener('click', allrow)
+function allrow(){
+    var $inputs = $('table').find('input');
+    $inputs.prop('checked', 'checked');
+}
+
+function showremovebtn(){
+    var checkBox = document.getElementById("Check");
+    var allcheckBox = document.getElementById("all");
+    var BtnREmove = document.getElementById("removeBtn");
+    if (checkBox.checked == true || allcheckBox.checked == true){
+        BtnREmove.style.display = "block";
+    } else {
+        BtnREmove.style.display = "none";
     }
 }
