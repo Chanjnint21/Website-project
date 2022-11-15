f1f57d7d828f9fc37e0f09de68d9cf747f3af9b9
Chanjnint21 <limchanjnint@gmail.com>
2022-11-15 16:33:15 +0700

    added action btn

--- assets/components/Table/InboxTable.js
+++ assets/components/Table/InboxTable.js
@@ -17,8 +17,8 @@ $(document).ready(function () {
     var lastname = localStorage.getItem("lastname");
     var title = localStorage.getItem("title");
     var subdate = localStorage.getItem("submitdate");
-    var pending_review = "<img src='https://icons.veryicon.com/png/o/education-technology/smart-campus-1/view-details-2.png' type='button' data-toggle='modal' data-target='#modal-pending'/>";
-    var Already_review = "<img src='https://icons.veryicon.com/png/o/education-technology/smart-campus-1/view-details-2.png' type='button' data-toggle='modal' data-target='#modal-reviewed'/>";
+    var pending_review = "<button data-title='view details'><img src='https://icons.veryicon.com/png/o/education-technology/smart-campus-1/view-details-2.png' type='button' data-toggle='modal' data-target='#modal-pending'/></button >";
+    var Already_review = "<button data-title='view details'><img src='https://icons.veryicon.com/png/o/education-technology/smart-campus-1/view-details-2.png' type='button' data-toggle='modal' data-target='#modal-reviewed'/></button >";
     var status = localStorage.getItem("status");
     // $('#btnn').on('click', function () {
     //     if (localStorage.getItem("firstname") === null){
@@ -38,38 +38,48 @@ $(document).ready(function () {
     var approve = "<span style='color: green;'>APPROVE</span>"; 
     var reject = "<span style='color: red;'>REJECT</span>";
     if (status == null){
-        var num2 = 1;
-        var num3 = 2;
-        var num4 = 3;
-        var num5 = 4;
-        var num6 = 5;
-        var num7 = 6;
-        var num8 = 7;
-        var num9 = 8;
-        var num10 = 9;
+        // var num2 = 1;
+        // var num3 = 2;
+        // var num4 = 3;
+        // var num5 = 4;
+        // var num6 = 5;
+        // var num7 = 6;
+        // var num8 = 7;
+        // var num9 = 8;
+        // var num10 = 9;
+        for (let i=1; i<11 ; i++){
+            t.row.add([i, 'Sokha', "chan", "Phcum Ben", "12/07/2022", approve, Already_review]).draw(false);
+        }
+        for (let j=11; j<21 ; j++){
+            t.row.add([j, 'MArk', "jackson", "title", "12/06/2022", reject, Already_review]).draw(false);
+        }
     } else {
         var num1 = 1;
-        var num2 = 2;
-        var num3 = 3;
-        var num4 = 4;
-        var num5 = 5;
-        var num6 = 6;
-        var num7 = 7;
-        var num8 = 8;
-        var num9 = 9;
-        var num10 = 10;
-        t.row.add([num1, firstname, lastname, title, subdate, pending_review, status]).draw(false);
+        // var num2 = 2;
+        // var num3 = 3;
+        // var num4 = 4;
+        // var num5 = 5;
+        // var num6 = 6;
+        // var num7 = 7;
+        // var num8 = 8;
+        // var num9 = 9;
+        // var num10 = 10;
+        t.row.add([num1, firstname, lastname, title, subdate, status, pending_review]).draw(false);
+        for (var i=2; i<12 ; i++){
+            t.row.add([i, 'Sokha', "chan", "Phcum Ben", "12/07/2022", approve, Already_review]).draw(false);
+        }
+        for (let j=11; j<21 ; j++){
+            t.row.add([j, 'MArk', "jackson", "title", "12/06/2022", reject, Already_review]).draw(false);
+        }
     }
-
-    // Automatically add a first row of data
-    t.row.add([num2, 'Sokha', "chan", "Phcum Ben", "12/07/2022", Already_review, approve]).draw(false);
-    t.row.add([num3, 'MArk', "jackson", "title", "12/06/2022", Already_review, approve]).draw(false);
-    t.row.add([num4, 'Thida', "karl", "Phcum Ben", "12/07/2022", Already_review, approve]).draw(false);
-    t.row.add([num5, 'Daro', "krosa", "title", "12/05/2022", Already_review, reject]).draw(false);
-    t.row.add([num6, 'vireak', "chan", "Phcum Ben", "12/09/2022", Already_review, reject]).draw(false);
-    t.row.add([num7, 'Kosal', "vicheka", "title", "13/07/2022", Already_review, reject]).draw(false);
-    t.row.add([num8, 'Kjoool', "Rosa", "title", "24/05/2022", Already_review, reject]).draw(false);
-    // t.row.add([num9, 'forlto', "chanao", "Phcum Ben", "31/09/2022", Already_review, reject]).draw(false);
-    // t.row.add([num10, 'candaal', "ghiloo", "title", "12/10/2022", Already_review, reject]).draw(false);
+    // t.row.add([i, 'Sokha', "chan", "Phcum Ben", "12/07/2022", approve, Already_review]).draw(false);
+    // t.row.add([j, 'MArk', "jackson", "title", "12/06/2022", reject, Already_review]).draw(false);
+    // t.row.add([num4, 'Thida', "karl", "Phcum Ben", "12/07/2022", approve, Already_review]).draw(false);
+    // t.row.add([num5, 'Daro', "krosa", "title", "12/05/2022", reject, Already_review]).draw(false);
+    // t.row.add([num6, 'vireak', "chan", "Phcum Ben", "12/09/2022", reject, Already_review]).draw(false);
+    // t.row.add([num7, 'Kosal', "vicheka", "title", "13/07/2022",reject, Already_review]).draw(false);
+    // t.row.add([num8, 'Kjoool', "Rosa", "title", "24/05/2022", reject, Already_review]).draw(false);
+    // t.row.add([num9, 'forlto', "chanao", "Phcum Ben", "31/09/2022", reject, Already_review]).draw(false);
+    // t.row.add([num10, 'candaal', "ghiloo", "title", "12/10/2022", reject, Already_review]).draw(false);
     
 });
--- assets/components/Table/Requestbox.js
+++ assets/components/Table/Requestbox.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     var lastname = localStorage.getItem("lastname");
     var title = localStorage.getItem("title");
     var subdate = localStorage.getItem("submitdate");
-    var review = "<img src='https://icons.veryicon.com/png/o/education-technology/smart-campus-1/view-details-2.png' type='button' data-toggle='modal' data-target='#modal-reviewed'/>";
+    var review = "<button data-title='review'><img src='https://icons.veryicon.com/png/o/education-technology/smart-campus-1/view-details-2.png' type='button' data-toggle='modal' data-target='#modal-reviewed'/></button ><button data-title='delete'><span class='material-icons'>delete</span></button >";
     var localstatus = localStorage.getItem("status");
     var status = "<button data-toggle='modal' data-target='#comfirm-modal' data-title='change status' ><span id='demo1' value=''>PENDING</span></button>";
     var approve = "<span style='color: green;'>APPROVE</span>"; 
@@ -21,34 +21,29 @@ $(document).ready(function () {
     var checkbox= "<input  type='checkbox' name= 'checkedbox' id='Check' onclick='showremovebtn()' class='select'/>";
     
     if (localstatus !== null){
-        t.row.add([checkbox, firstname, lastname, title, subdate, review, status]).draw(false);
+        t.row.add([checkbox, firstname, lastname, title, subdate, status, review]).draw(false);
     } 
     // Automatically add a first row of data
 
-    t.row.add([checkbox, 'Sokha', "chan", "Phcum Ben", "12/07/2022", review, approve]).draw(false);
-    t.row.add([checkbox, 'MArk', "jackson", "title", "12/06/2022", review, approve]).draw(false);
-    t.row.add([checkbox, 'Thida', "karl", "Phcum Ben", "12/07/2022", review, approve]).draw(false);
-    t.row.add([checkbox, 'Daro', "krosa", "title", "12/05/2022", review, reject]).draw(false);
-    t.row.add([checkbox, 'vireak', "chan", "Phcum Ben", "12/09/2022", review, reject]).draw(false);
-    t.row.add([checkbox, 'Kosal', "vicheka", "title", "13/07/2022",review, reject]).draw(false);
-    t.row.add([checkbox, 'Kjoool', "Rosa", "title", "24/05/2022", review, reject]).draw(false);
+    t.row.add([checkbox, 'Sokha', "chan", "Phcum Ben", "12/07/2022", approve, review]).draw(false);
+    t.row.add([checkbox, 'MArk', "jackson", "title", "12/06/2022", approve, review]).draw(false);
+    t.row.add([checkbox, 'Thida', "karl", "Phcum Ben", "12/07/2022", approve, review]).draw(false);
+    t.row.add([checkbox, 'Daro', "krosa", "title", "12/05/2022", reject, review]).draw(false);
+    t.row.add([checkbox, 'vireak', "chan", "Phcum Ben", "12/09/2022", reject, review]).draw(false);
+    t.row.add([checkbox, 'Kosal', "vicheka", "title", "13/07/2022",reject, review]).draw(false);
+    t.row.add([checkbox, 'Kjoool', "Rosa", "title", "24/05/2022", reject, review]).draw(false);
+    t.row.add([checkbox, 'forlto', "chanao", "Phcum Ben", "31/09/2022", reject, review]).draw(false);
+    t.row.add([checkbox, 'candaal', "ghiloo", "title", "12/10/2022", reject, review]).draw(false);
     //
-    t.row.add([checkbox, 'Sokha', "chan", "Phcum Ben", "12/07/2022", review, approve]).draw(false);
-    t.row.add([checkbox, 'MArk', "jackson", "title", "12/06/2022", review, approve]).draw(false);
-    t.row.add([checkbox, 'Thida', "karl", "Phcum Ben", "12/07/2022", review, approve]).draw(false);
-    t.row.add([checkbox, 'Daro', "krosa", "title", "12/05/2022", review, reject]).draw(false);
-    t.row.add([checkbox, 'vireak', "chan", "Phcum Ben", "12/09/2022", review, reject]).draw(false);
-    t.row.add([checkbox, 'Kosal', "vicheka", "title", "13/07/2022",review, reject]).draw(false);
-    t.row.add([checkbox, 'Kjoool', "Rosa", "title", "24/05/2022", review, reject]).draw(false);
-    t.row.add([checkbox, 'Sokha', "chan", "Phcum Ben", "12/07/2022", review, approve]).draw(false);
-    t.row.add([checkbox, 'MArk', "jackson", "title", "12/06/2022", review, approve]).draw(false);
-    t.row.add([checkbox, 'Thida', "karl", "Phcum Ben", "12/07/2022", review, approve]).draw(false);
-    t.row.add([checkbox, 'Daro', "krosa", "title", "12/05/2022", review, reject]).draw(false);
-    t.row.add([checkbox, 'vireak', "chan", "Phcum Ben", "12/09/2022", review, reject]).draw(false);
-    t.row.add([checkbox, 'Kosal', "vicheka", "title", "13/07/2022",review, reject]).draw(false);
-    t.row.add([checkbox, 'Kjoool', "Rosa", "title", "24/05/2022", review, reject]).draw(false);
-    // t.row.add([checkbox, 'forlto', "chanao", "Phcum Ben", "31/09/2022", review, reject]).draw(false);
-    // t.row.add([checkbox, 'candaal', "ghiloo", "title", "12/10/2022", review, reject]).draw(false);
+    t.row.add([checkbox, 'Sokha', "chan", "Phcum Ben", "12/07/2022", approve, review]).draw(false);
+    t.row.add([checkbox, 'MArk', "jackson", "title", "12/06/2022", approve, review]).draw(false);
+    t.row.add([checkbox, 'Thida', "karl", "Phcum Ben", "12/07/2022", approve, review]).draw(false);
+    t.row.add([checkbox, 'Daro', "krosa", "title", "12/05/2022", reject, review]).draw(false);
+    t.row.add([checkbox, 'vireak', "chan", "Phcum Ben", "12/09/2022", reject, review]).draw(false);
+    t.row.add([checkbox, 'Kosal', "vicheka", "title", "13/07/2022",reject, review]).draw(false);
+    t.row.add([checkbox, 'Kjoool', "Rosa", "title", "24/05/2022", reject, review]).draw(false);
+    t.row.add([checkbox, 'forlto', "chanao", "Phcum Ben", "31/09/2022", reject, review]).draw(false);
+    t.row.add([checkbox, 'candaal', "ghiloo", "title", "12/10/2022", reject, review]).draw(false);
     
 
     document.getElementById("Reject").addEventListener("click", Rbtn)
@@ -81,7 +76,6 @@ function status1(){
     }
 }
 
-
 function DeleteRow() {
     document.querySelectorAll('#display .select:checked').forEach(e => {
       e.parentNode.parentNode.remove();
@@ -94,21 +88,21 @@ function DeleteRow() {
 //     selectall.prop('checked', 'checked');
 // }
 
-function checkUncheck(checkBox) {
-    var get = document.getElementsByName('checkedbox');
-    for(var i=0; i<get.length; i++) {
-        get[i].checked = checkBox.checked;
-    }
-}
+// function checkUncheck(checkBox) {
+//     var get = document.getElementsByName('checkedbox');
+//     for(var i=0; i<get.length; i++) {
+//         get[i].checked = checkBox.checked;
+//     }
+// }
 
-function showremovebtn(){
-    // let checkBox = document.getElementsByClassName("select");
-    // let checkBoxx = document.getElementsByName('checkedbox');
-    let checkBox = document.getElementById('Check');
-    var BtnRemove = document.getElementById("removeBtn");
-        if (checkBox.checked == true){
-            BtnRemove.style.display = "block";
-        } else {
-            BtnRemove.style.display = "none";
-        }
-}
+// function showremovebtn(){
+//     // let checkBox = document.getElementsByClassName("select");
+//     // let checkBoxx = document.getElementsByName('checkedbox');
+//     let checkBox = document.getElementById('Check');
+//     var BtnRemove = document.getElementById("removeBtn");
+//         if (checkBox.checked == true){
+//             BtnRemove.style.display = "block";
+//         } else {
+//             BtnRemove.style.display = "none";
+//         }
+// }
