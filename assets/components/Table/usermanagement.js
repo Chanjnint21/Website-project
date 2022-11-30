b241372f90f69a938ad16c0e9d5f2ed0d88a54e8
Chanjnint21 <limchanjnint@gmail.com>
2022-11-30 17:08:47 +0700

    added new user and delete user (In admin table)

--- assets/components/Table/usermanagement.js
+++ assets/components/Table/usermanagement.js
@@ -1,8 +1,20 @@
 //auto sort and pagination
-var t = $('#UM').DataTable();
+var t = $('#UsM').DataTable();
 
-var action = "<span class='material-icons' title='Edit' data-toggle='tooltip'>edit</span><span class='material-icons text-danger' title='Delete' data-toggle='tooltip'>cancel</span>";
-let num = 0;
+const new_Fname = localStorage.getItem("new-Fname");
+const new_Lname =localStorage.getItem("new-Lname");
+const new_Role = localStorage.getItem("new-role");
+const new_phone = localStorage.getItem("new-phone");
+const new_email = localStorage.getItem("new-email");
+const action = "<button><span class='material-icons' title='Edit' data-toggle='tooltip'>edit</span></button> <button  data-toggle='modal' data-target='#RemoveUser-modal'><span class='material-icons text-danger' title='Delete' data-toggle='tooltip' >cancel</span></button>";
+var newuser = localStorage.getItem("new-email");
+let num;
+if (newuser !== null){
+    num = 1;
+    t.row.add([num + 0, new_Fname, new_Lname, new_Role, new_phone, new_email, action]).draw(false);
+} else {
+    num = 0;
+}
 
 //auto added data into table
 t.row.add([num + 1, 'Sokha', "chan", "Admin", "Phone numbe", "Email", action]).draw(false);
@@ -34,7 +46,7 @@ function createuser() {
     if (pass == Cpass){
         localStorage.setItem("new-Fname", document.getElementById("new-Fname").value);
         localStorage.setItem("new-Lname", document.getElementById("new-Lname").value);
-        localStorage.setItem("nnew-phone", document.getElementById("new-phone").value);
+        localStorage.setItem("new-phone", document.getElementById("new-phone").value);
         localStorage.setItem("new-email", document.getElementById("new-email").value);
         localStorage.setItem("new-pass", Cpass);
         //check which radio is check
@@ -47,6 +59,11 @@ function createuser() {
         document.getElementById("cf-password-validations").innerHTML= "confirm password does not match";
     }
 }
- 
-var New_Email = localStorage.getItem("new-email");
-var New_pass = localStorage.get
+
+function RemoveUser(){
+    let keysToRemove = ["new-Fname", "new-Lname", "new-phone", "new-pass", "new-role", "new-email"];
+    for (key of keysToRemove) {
+        localStorage.removeItem(key);
+        location.reload();
+    }
+}
