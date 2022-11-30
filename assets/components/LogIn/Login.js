44d356b2d6e54b84b55f83dcd7cbf6aa43979633
Chanjnint21 <limchanjnint@gmail.com>
2022-11-30 17:06:19 +0700

    fixed new user log in problem

--- assets/components/LogIn/Login.js
+++ assets/components/LogIn/Login.js
@@ -25,11 +25,9 @@ function LogIn(){
     Login_form.addEventListener('submit', function (e){
         e.preventDefault();
 
-        var logincard = document.getElementById("login-card");
-        var loader = document.getElementById("load");
         const newUser = localStorage.getItem("new-email");
         const newPass = localStorage.getItem("new-pass");
-
+        const role = localStorage.getItem("new-role");
 
         const user1 = "adminpage"
         const pass1 = 12345;
@@ -38,45 +36,44 @@ function LogIn(){
         const pass2 = 54321;
 
         if (user1 == inputname && pass1 == inputpassword){
-            switchtoAdmin();
-        } else if (user2 == inputname && pass2 == inputpassword) {
-            switchtoUser();
-        } else if(newUser == inputname && newPass == inputpassword){
+            var logincard = document.getElementById("login-card");
+            var loader = document.getElementById("load");
             logincard.style.display = "none";
             loader.style.display = "block";
-            timeout = setTimeout(switchpage, 5000);
+            timeout = setTimeout(switchtoAdmin, 3000);
+            // switchtoAdmin();
+        } else if (user2 == inputname && pass2 == inputpassword) {
+            var logincard = document.getElementById("login-card");
+            var loader = document.getElementById("load");
+            logincard.style.display = "none";
+            loader.style.display = "block";
+            timeout = setTimeout(switchtoUser, 3000);
+            // switchtoUser();
+        } else if(newUser == inputname && newPass == inputpassword){
+            if(role == "Admin"){
+                var logincard = document.getElementById("login-card");
+                var loader = document.getElementById("load");
+                logincard.style.display = "none";
+                loader.style.display = "block";
+                timeout = setTimeout(switchtoAdmin, 2000);
+            } else {
+                var logincard = document.getElementById("login-card");
+                var loader = document.getElementById("load");
+                logincard.style.display = "none";
+                loader.style.display = "block";
+                timeout = setTimeout(switchtoUser, 2000);
+            }
         } else {
-            document.getElementById("Invalid_inp").innerHTML= "*Invalid email or password*"
+            document.getElementById("Invalid_inp").innerHTML= "*Invalid email or password*";
         }
     } )   
 }
 
-function switchpage(){
-    var role = localStorage.getItem("new-role");
-    if (role == "admin"){
-        window.location.href = '../Admin-page/MainPage.html';
-    }
-}
-
 function switchtoUser() {
-    var logincard = document.getElementById("login-card");
-    var loader = document.getElementById("load");
-    logincard.style.display = "none";
-    loader.style.display = "block";
-    timeout = setTimeout(switchtoUser, 1000);
-    if(timeout == 3){
-            window.location.href = '../User-page/UserMainPage.html';
-    }
+    window.location.href = '../User-page/UserMainPage.html';
   }
 
 function switchtoAdmin() {
-    var logincard = document.getElementById("login-card");
-    var loader = document.getElementById("load");
-    logincard.style.display = "none";
-    loader.style.display = "block";
-    timeout = setTimeout(switchtoAdmin, 1000);
-    if(timeout == 3){
-            window.location.href = '../Admin-page/MainPage.html';
-    }
+    window.location.href = '../Admin-page/MainPage.html';
   }
 
