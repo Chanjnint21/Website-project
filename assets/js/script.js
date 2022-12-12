e2981ccaed637e69715da0e33fc8910156b53c8c
Chanjnint21 <limchanjnint@gmail.com>
2022-12-12 17:17:57 +0700

    remove form script

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,65 +33,10 @@ function countdays () {
     document.getElementById("noOfDays").setAttribute("value", isNaN(countdays) ? '' : countdays);
     document.getElementById("noOfDays").innerHTML = countdays + "days";
 }
-
-//--------------------------------------------------- store form input into local storage ---------------------------------------------
-function movepage(){
-    // Parse the JSON stored in allEntriesP
-    var InboxEntries = JSON.parse(localStorage.getItem("ToInbox"));
-    if(InboxEntries == null) InboxEntries = [];
-    var RequestEntries = JSON.parse(localStorage.getItem("ToRequestbox"));
-    if(RequestEntries == null) RequestEntries = [];
-
-    var inputFname= document.getElementById("firstname").value;
-    var inputLname= document.getElementById("lastname").value;
-    var inputPhone= document.getElementById("phone").value;
-    var inputEmail= document.getElementById("email").value;
-    var inputLeaveDate= document.getElementById("leave_date").value  + ' to ' + document.getElementById("back_date").value;
-    var inputTotalleave= document.getElementById("noOfDays").value;
-    var inputtitle= document.getElementById("title").value;
-    var inputReason= document.getElementById("reason").value;
-    var inputattachment= document.getElementById("attachment").value;
-    var status = "PENDING";
-
-    //Get the date of submit form event
-        const event = new Date();
-        const years = {  year: 'numeric'};
-        const months = { month: 'numeric' };
-        const days = { day: 'numeric' };
-        
-        const year = event.toLocaleDateString('en-Us', years);
-        const month = event.toLocaleDateString('en-Us', months);
-        const day = event.toLocaleDateString('en-Us', days);
-        
-        const submitDate = day + "/" + month + "/" + year;
-        var current_time = event.getHours()+":"+event.getMinutes()+":"+ event.getSeconds();
-
-    const entry = {
-        "InpFname": inputFname,
-        "InpLname": inputLname,
-        "Inpphone": inputPhone,
-        "InpEmail": inputEmail,
-        "InpLeaveDate": inputLeaveDate,
-        "InpTotalleave": inputTotalleave,
-        "Inptitle": inputtitle,
-        "InpReason": inputReason,
-        "Inpattachment": inputattachment,
-        "Inpstatus": status,
-        "InpSubmitday": submitDate,
-        "Inptime" : current_time,
-    };
-
-    // Save allEntries back to local storage
-    InboxEntries.unshift(entry);
-    RequestEntries.unshift(entry);
-    localStorage.setItem("ToInbox", JSON.stringify(InboxEntries));
-    localStorage.setItem("ToRequestbox", JSON.stringify(RequestEntries));
-    //load to inbox page
-    window.location.href = "./Inbox.html";
-}
 //------------------------------------------------------- Logout ---------------------------------------------------
 
 function LogOut(){
+    localStorage.removeItem("Login User")
     var wrappe = document.getElementById("wrapper");
     var loader = document.getElementById("loading");
     wrappe.style.display = "none";
@@ -124,3 +69,5 @@ function showPage() {
         document.getElementById("myDiv").style.display = "block";
     }
 
+var logginuser = JSON.parse(localStorage.getItem("Login User"))
+document.getElementById('UserPageName').innerHTML=  logginuser.Lname+" "+ logginuser.Fname;
