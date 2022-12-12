1d651c07ff488697e0dc5d3dc40205048923b8bb
Chanjnint21 <limchanjnint@gmail.com>
2022-12-12 17:17:37 +0700

    make a toggle between modal

--- assets/components/Table/InboxTable.js
+++ assets/components/Table/InboxTable.js
@@ -18,11 +18,7 @@ $('#display').dataTable( {
         {'data': 'Inptitle'},
         {'data': 'InpSubmitday'},
         {'data': 'Inpstatus'},
-        {"data": "id",
-            "render": function ( data, type, row, meta ) { 
-                return pending_review
-                }
-        }
+        {"data": "pendingmodal"}
         
      ]
 } )
@@ -49,12 +45,32 @@ $('#display tbody').on( 'click', 'tr',  (event) =>  {
     document.getElementById("In_email").value = data.InpEmail;
     document.getElementById("In_totalleave").value = data.InpTotalleave + " day(s)";
     document.getElementById("In_leavedate").value = data.InpSubmitday;
+    document.getElementById("In_title").value = data.Inptitle;
     document.getElementById("In_reason").value = data.InpReason;
     document.getElementById("In_time").innerHTML = data.Inptime;
     if (data.Inpattachment.value == null){
         document.getElementById("In_attachment").value = "None";
     } else {
         document.getElementById("In_attachment").value = data.Inpattachment;
+    }
+});
+
+$('#display tbody').on( 'click', 'tr',  (event) =>  {
+    var data = table.row( event.currentTarget ).data()
+    document.getElementById("ReviewModalTitle").innerHTML = data.InpFname + "'s Request form";
+    document.getElementById("RIn_Fname").innerHTML = data.InpFname;
+    document.getElementById("RIn_Lname").innerHTML = data.InpLname;
+    document.getElementById("RIn_phone").innerHTML = data.Inpphone;
+    document.getElementById("RIn_email").innerHTML = data.InpEmail;
+    document.getElementById("RIn_totalleave").innerHTML = data.InpTotalleave + " day(s)";
+    document.getElementById("RIn_leavedate").innerHTML = data.InpSubmitday;
+    document.getElementById("RIn_title").innerHTML = data.Inptitle;
+    document.getElementById("RIn_reason").innerHTML = data.InpReason;
+    document.getElementById("RIn_time").innerHTML = data.Inptime;
+    if (data.Inpattachment.innerHTML == null){
+        document.getElementById("RIn_attachment").innerHTML = "None";
+    } else {
+        document.getElementById("RIn_attachment").innerHTML = data.Inpattachment;
     }
 });
 
@@ -84,3 +100,56 @@ function deleteInrequestbox(){
         }
     }
 }
+// function savechange(){
+//     var Inbox_listData = JSON.parse(localStorage.getItem("ToInbox"));
+//     var Usertime = document.getElementById("req_time").innerHTML;
+//     var UserFname = document.getElementById("In_fname").value;
+//     var UserLname = document.getElementById("In_lname").value;
+//     var UserPhone = document.getElementById("In_phone").value ;
+//     var UserEmail = document.getElementById("In_email").value ;
+//     var UserTotalleave = document.getElementById("In_totalleave").value;
+//     var Usersubmitdat = document.getElementById("In_leavedate").value;
+//     var Usertitle = document.getElementById("In_title").value;
+//     var Usserreason = document.getElementById("In_reason").value;
+//     var Usertime = document.getElementById("In_time").innerHTML;
+//     var inputattachment= document.getElementById("In_attachment").value;
+//     var status = "PENDING";
+
+//     const event = new Date();
+//     const years = {  year: 'numeric'};
+//     const months = { month: 'numeric' };
+//     const days = { day: 'numeric' };
+    
+//     const year = event.toLocaleDateString('en-Us', years);
+//     const month = event.toLocaleDateString('en-Us', months);
+//     const day = event.toLocaleDateString('en-Us', days);
+    
+//     const submitDate = day + "/" + month + "/" + year;
+//     var current_time = event.getHours()+":"+event.getMinutes()+":"+ event.getSeconds();
+
+//     var after_review = "<button title='view detail' data-toggle='tooltip'><span class='material-icons' data-toggle='modal' data-target='#modal-reviewed'>visibility</span></button >";
+//     for (var i = 0; i <= Inbox_listData.length; i++ ){
+//         if ( Inbox_listData[i].Inptime == Usertime){
+//             const entry = {
+//                 "InpFname": UserFname,
+//                 "InpLname": UserLname,
+//                 "Inpphone": UserPhone,
+//                 "InpEmail": UserEmail,
+//                 "InpLeaveDate": UserTotalleave,
+//                 "InpTotalleave": Usersubmitdat,
+//                 "Inptitle": Usertitle,
+//                 "InpReason": Usserreason,
+//                 "Inpattachment": inputattachment,
+//                 "Inpstatus": status,
+//                 "InpSubmitday": submitDate,
+//                 "Inptime" : current_time,
+//                 "pendingmodal" : after_review,
+//             };
+//             Inbox_listData.push(entry);
+//             localStorage.setItem("ToInbox", JSON.stringify(Inbox_listData));
+//             Inbox_listData.splice(i, 1);
+//             localStorage.setItem('ToInbox', JSON.stringify(Inbox_listData));
+//             location.reload()
+//         }
+//     }
+// }
